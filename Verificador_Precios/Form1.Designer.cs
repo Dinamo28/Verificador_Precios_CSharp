3b8995f60b4d6702fc5b8d29b04d1ba1ab0f4b25
Dinamo28 <bruno281299@gmail.com>
2021-09-29 07:56:05 -0700

    Agregar archivos de proyecto.

---
+++ Verificador_Precios/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace Verificador_Precios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.Logo = new System.Windows.Forms.PictureBox();
+            this.Aviso = new System.Windows.Forms.Label();
+            this.Lector = new System.Windows.Forms.PictureBox();
+            this.Datos = new System.Windows.Forms.Label();
+            this.ImagenP = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Cargar = new System.Windows.Forms.PictureBox();
+            this.Demo = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.ErrorImg = new System.Windows.Forms.PictureBox();
+            this.AvisoError = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lector)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagenP)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cargar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Demo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // Logo
+            // 
+            this.Logo.BackColor = System.Drawing.Color.Transparent;
+            this.Logo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Logo.BackgroundImage")));
+            this.Logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Logo.Location = new System.Drawing.Point(39, 31);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(474, 285);
+            this.Logo.TabIndex = 0;
+            this.Logo.TabStop = false;
+            // 
+            // Aviso
+            // 
+            this.Aviso.AutoSize = true;
+            this.Aviso.BackColor = System.Drawing.Color.Transparent;
+            this.Aviso.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Aviso.Location = new System.Drawing.Point(157, 205);
+            this.Aviso.Name = "Aviso";
+            this.Aviso.Size = new System.Drawing.Size(332, 33);
+            this.Aviso.TabIndex = 1;
+            this.Aviso.Text = "Esperando por articulo...";
+            this.Aviso.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lector
+            // 
+            this.Lector.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Lector.BackgroundImage")));
+            this.Lector.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Lector.Image = ((System.Drawing.Image)(resources.GetObject("Lector.Image")));
+            this.Lector.Location = new System.Drawing.Point(331, 278);
+            this.Lector.Name = "Lector";
+            this.Lector.Size = new System.Drawing.Size(214, 139);
+            this.Lector.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Lector.TabIndex = 2;
+            this.Lector.TabStop = false;
+            // 
+            // Datos
+            // 
+            this.Datos.AutoSize = true;
+            this.Datos.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Datos.Location = new System.Drawing.Point(186, 283);
+            this.Datos.Name = "Datos";
+            this.Datos.Size = new System.Drawing.Size(99, 33);
+            this.Datos.TabIndex = 3;
+            this.Datos.Text = "label2";
+            this.Datos.Visible = false;
+            this.Datos.Click += new System.EventHandler(this.Datos_Click);
+            // 
+            // ImagenP
+            // 
+            this.ImagenP.BackColor = System.Drawing.Color.Transparent;
+            this.ImagenP.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ImagenP.Location = new System.Drawing.Point(636, 294);
+            this.ImagenP.Margin = new System.Windows.Forms.Padding(2);
+            this.ImagenP.Name = "ImagenP";
+            this.ImagenP.Size = new System.Drawing.Size(400, 400);
+            this.ImagenP.TabIndex = 4;
+            this.ImagenP.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Cargar
+            // 
+            this.Cargar.BackColor = System.Drawing.Color.Transparent;
+            this.Cargar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Cargar.InitialImage = null;
+            this.Cargar.Location = new System.Drawing.Point(291, 94);
+            this.Cargar.Name = "Cargar";
+            this.Cargar.Size = new System.Drawing.Size(200, 200);
+            this.Cargar.TabIndex = 5;
+            this.Cargar.TabStop = false;
+            this.Cargar.Click += new System.EventHandler(this.Cargar_Click);
+            // 
+            // Demo
+            // 
+            this.Demo.BackColor = System.Drawing.Color.Transparent;
+            this.Demo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Demo.InitialImage = null;
+            this.Demo.Location = new System.Drawing.Point(535, 31);
+            this.Demo.Name = "Demo";
+            this.Demo.Size = new System.Drawing.Size(400, 300);
+            this.Demo.TabIndex = 6;
+            this.Demo.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // ErrorImg
+            // 
+            this.ErrorImg.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ErrorImg.BackgroundImage")));
+            this.ErrorImg.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ErrorImg.Location = new System.Drawing.Point(39, 342);
+            this.ErrorImg.Name = "ErrorImg";
+            this.ErrorImg.Size = new System.Drawing.Size(400, 400);
+            this.ErrorImg.TabIndex = 7;
+            this.ErrorImg.TabStop = false;
+            // 
+            // AvisoError
+            // 
+            this.AvisoError.AutoSize = true;
+            this.AvisoError.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+            this.AvisoError.Location = new System.Drawing.Point(68, 330);
+            this.AvisoError.Name = "AvisoError";
+            this.AvisoError.Size = new System.Drawing.Size(99, 33);
+            this.AvisoError.TabIndex = 8;
+            this.AvisoError.Text = "label1";
+            this.AvisoError.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AvisoError);
+            this.Controls.Add(this.ErrorImg);
+            this.Controls.Add(this.Demo);
+            this.Controls.Add(this.Cargar);
+            this.Controls.Add(this.ImagenP);
+            this.Controls.Add(this.Datos);
+            this.Controls.Add(this.Lector);
+            this.Controls.Add(this.Aviso);
+            this.Controls.Add(this.Logo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lector)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagenP)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cargar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Demo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorImg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.PictureBox Logo;
+        private System.Windows.Forms.Label Aviso;
+        private System.Windows.Forms.PictureBox Lector;
+        private System.Windows.Forms.Label Datos;
+        private System.Windows.Forms.PictureBox ImagenP;
+		private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Demo;
+        private System.Windows.Forms.PictureBox Cargar;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox ErrorImg;
+        private System.Windows.Forms.Label AvisoError;
+    }
+}
+
---
+++ Verificador_Precios/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Verificador_Precios
+{
+    public partial class Form1 : Form
+    {
+        private int segundos = 0;
+        private int segundos2 = 0;
+        private String codigo = "";
+        public Form1()
+        {
+            InitializeComponent();
+            Cargar.BackColor = Color.Transparent;
+            Demo.BackColor = Color.Transparent;
+            Logo.BackColor = Color.Transparent;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AvisoError.Text = "No se pudo escanear el \nproducto, Intente de nuevo o \npase a servicio al cliente";
+
+            Logo.Location = new Point(this.Width / 4 - Logo.Width / 2, this.Height/3-Logo.Height/2);
+            Demo.Location = new Point(3*this.Width / 4 - Demo.Width / 2, this.Height / 3 - Demo.Height / 2);
+            Lector.Location = new Point(this.Width/2 - Lector.Width/2,this.Height-Lector.Height-50);
+            Aviso.Location = new Point(this.Width / 2 - Aviso.Width / 2, this.Height-Lector.Height - 90);
+            ErrorImg.Location = new Point(this.Width / 2 - ErrorImg.Width / 2, this.Height / 2 - ErrorImg.Height / 2 - 100);
+            AvisoError.Location = new Point(this.Width / 2 - AvisoError.Width/2, this.Height/2 - AvisoError.Height+200);
+            Cargar.Location = new Point(this.Width / 2 - Cargar.Width / 2, this.Height/2 - Cargar.Height/2);
+            ImagenP.Location = new Point(this.Width/8,this.Height/2-ImagenP.Height+100);
+            Datos.Location = new Point(this.Width / 2, ImagenP.Location.Y);
+
+            Cargar.Image = Image.FromFile("C:\\Users\\teo_2\\source\\repos\\Verificador_Precios\\Verificador_Precios\\img\\loading-waiting.gif");
+            Demo.Image = Image.FromFile("C:\\Users\\teo_2\\source\\repos\\Verificador_Precios\\Verificador_Precios\\img\\LectorGif.gif");
+            Cargar.SizeMode = PictureBoxSizeMode.StretchImage;
+            Demo.SizeMode = PictureBoxSizeMode.StretchImage;
+            Cargar.BackColor = Color.Transparent;
+            Demo.BackColor = Color.Transparent;
+
+            ImagenP.Visible = false;
+            Cargar.Visible = false;
+            ErrorImg.Visible = false;
+            AvisoError.Visible = false;
+
+        }
+
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                //MessageBox.Show("vamos a buscar el producto "+codigo);
+                Datos.Visible = false;
+                AvisoError.Visible = false;
+                ErrorImg.Visible = false;
+                ImagenP.Visible = false;
+                Logo.Height = 95;
+                Logo.Width = 158;
+                Logo.Location = new Point(this.Width / 4 - Logo.Width / 2, this.Height - Logo.Height - 50);
+                Demo.Visible = false;
+                Aviso.Visible = false;
+                Lector.Visible = false;
+                Cargar.Visible = true;
+                segundos2 = 0;
+                timer2.Enabled = true;
+
+                try
+                {
+                    MySqlConnection servidor;
+                    servidor = new MySqlConnection("server = 127.0.0.1; user = root; database = verificador_precios; SSL Mode = None; ");
+                    servidor.Open();
+                    string query = "SELECT producto_nombre, producto_precio, producto_stock, producto_imagen FROM productos WHERE producto_codigo =" + codigo + ";";
+                    //MessageBox.Show(query);
+                    MySqlCommand consulta;
+                    consulta = new MySqlCommand(query, servidor);
+                    MySqlDataReader resultado = consulta.ExecuteReader();
+
+                    if (resultado.HasRows)
+                    {
+                        resultado.Read();
+                        Demo.Visible = false;
+                        Datos.Visible = true;
+                        Lector.Visible=true;
+                        ImagenP.Visible = true;
+                        Logo.Visible = true;
+                        Aviso.Visible = true;
+                        Aviso.Text = "Esperando por nuevo articulo";
+                        Aviso.Location = new Point(this.Width / 2 - Aviso.Width / 2, this.Height - Lector.Height - 90);
+                        Datos.Text = resultado.GetString(0)+Environment.NewLine+"Precio: $"+resultado.GetString(1) +
+                            Environment.NewLine + "Stock:" + resultado.GetString(2);
+                        ImagenP.ImageLocation = resultado.GetString(3);
+                        ImagenP.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                        segundos = 0;
+                        timer1.Enabled = true;
+                    }
+                    else
+                    {
+                        Lector.Visible = true;
+                        ErrorImg.Visible = true;
+                        Aviso.Visible = true;
+                        AvisoError.Visible = true;
+                        Datos.Visible = false;
+                        ImagenP.Visible = false;
+                        segundos = 0;
+                        timer1.Enabled = true;
+                    }
+                }
+                catch (Exception x)
+                {
+                    Datos.Visible = false;
+                    Lector.Visible = true;
+                    Aviso.Visible = true;
+                    ImagenP.Visible = false;
+                    ErrorImg.Visible = true;
+                    AvisoError.Visible = true;
+                    segundos = 0;
+                    timer1.Enabled = true;
+                }
+                codigo = "";
+            }
+            else
+            {
+                codigo += e.KeyChar;
+            }
+        }
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+            segundos++;
+
+            if (segundos == 8)
+            {
+                timer1.Enabled = false;
+                Lector.Visible = true;
+                ImagenP.Visible = false;
+                Logo.Height = 285;
+                Logo.Width = 474;
+                Logo.Location = new Point(this.Width / 4 - Logo.Width / 2, this.Height / 3 - Logo.Height / 2);
+                Demo.Visible = true;
+                Datos.Text = "";
+                Aviso.Visible = true;
+                Aviso.Text = "Esperando por articulo...";
+                Aviso.Location = new Point(this.Width / 2 - Aviso.Width / 2, this.Height - Lector.Height - 90);
+                ErrorImg.Visible = false;
+                AvisoError.Visible = false;
+
+            }
+		}
+
+        private void Cargar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            segundos2++;
+            if (segundos2==2)
+            {
+                timer2.Enabled = false;
+                Cargar.Visible = false;
+            }
+        }
+
+        private void Datos_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
